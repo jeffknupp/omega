ca1f2c255305526be50059cb7137d30ad36ebf26
Jeff Knupp <jknupp@appnexus.com>
2014-06-15 14:00:05 -0400

    new update

--- test/app.py
+++ test/app.py
@@ -1,7 +1,9 @@
 from whizbang.http.core import create_app
+from whizbang.http.resource import resource_definition
 from werkzeug import run_simple
 
 if __name__ == '__main__':
     app = create_app(__name__)
-    app.page('/', 'base.html')
+    #app.page('/', 'base.html')
+    app.resource('tweet', resource_definition('tweet'))
     run_simple('127.0.0.1', 5000, app, use_debugger=True, use_reloader=True)
